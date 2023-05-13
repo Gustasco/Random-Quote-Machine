b5a25c197c7feba53f9a3879349343eb1db83125
Gustavo_Espinoza <gustavo.espinoza.aliste@gmail.com>
2023-05-13 16:45:23 -0400

    inicio de proyecto de consumo de api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
+
+  const fetchQuote = async () => {
+    const response = await fetch("https://api.quotable.io/random");
+    const data = await response.json();
+    setQuote(data.content);
+    setAuthor(data.author);
+  };
+
+  useEffect(() => {
+    fetchQuote();
+  }, []);
+
+  const tweetQuote = () => {
+    const tweetUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+    window.open(tweetUrl, "_blank");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="quote-box">
+      <div id="text">{quote}</div>
+      <div id="author">- {author}</div>
+      <button id="new-quote" onClick={fetchQuote}>
+        New Quote
+      </button>
+      <a id="tweet-quote" href="#" onClick={tweetQuote}>
+        Tweet Quote
+        <img
+          id="twitter-logo"
+          src="https://static.vecteezy.com/system/resources/previews/018/930/745/non_2x/twitter-logo-twitter-icon-transparent-free-free-png.png"
+          alt="icono twitter"
+        ></img>
+      </a>
     </div>
   );
 }
